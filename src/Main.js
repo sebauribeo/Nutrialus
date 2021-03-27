b98a0f2efb53ce8701e24fe1e70217bd8cd7ebe2
sebauribeo <chefsebauribe@gmail.com>
2021-03-27 17:15:29 -0300

    visualizacion de api

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,33 @@
 import './main.css'
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+
 
 function Main() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [response, setItems] = useState([]);
+    
     useEffect(() => {
         fetch("https://0q27loouph.execute-api.us-east-1.amazonaws.com/")
-        .then((res) => res.json())
-        .then((response) => console.log(response))
+        .then(res => res.json())
+        .then((response) => {
+          console.log(response)
+          setIsLoaded(true);
+          setItems(response);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+         }
+        )
     }, [])
-    return (
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
         <div className="main">
 
             <header className="main__header">
@@ -25,17 +45,25 @@ function Main() {
             </header>
 
             <section className="main__section">
-                <img src="..." alt="..." className="main__img"></img>
+                <img src={response.image} alt="..." className="main__img"></img>
                 <div>
-                    <h5>Card title</h5>
-                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <button className="main__button">Go somewhere</button>
+                    <h5>Nombre: {response.name}</h5>
+                    <h5>Email: {response.email}</h5>
+                    <h5>Telefono: {response.phone}</h5>
+                    <a href="http://localhost:3000/" >
+                    <button className="main__button">Siguiente busqueda</button>
+                    </a>
                 </div>
             </section>
 
             
         </div>
-    )
-}
+
+      );
+    }
+  }
+    
+    
+
 
 export default Main
